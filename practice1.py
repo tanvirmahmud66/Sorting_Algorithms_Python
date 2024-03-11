5e7f67bf5c488afe13211db1cc825fe7333b8dc1
Tanvir Mahmud <tanvirmahmudfahim1313@gmail.com>
2024-03-11 11:13:22 +0600

    Sorting Technique

--- practice1.py
+++ practice1.py
@@ -1,3 +1,4 @@
+# ----------------------------------- Selection sort
 def selection(arr):
     n = len(arr)
     for i in range(n-1):
@@ -7,7 +8,7 @@ def selection(arr):
                 mini = j
         arr[i],arr[mini] = arr[mini], arr[i]
 
-
+# ----------------------------------- Bubble sort
 def bubble(arr):
     n = len(arr)
     for i in range(n):
@@ -15,7 +16,7 @@ def bubble(arr):
             if arr[j]>arr[j+1]:
                 arr[j],arr[j+1] = arr[j+1],arr[j]
 
-
+# ----------------------------------- Insertion sort
 def insertion(arr):
     n = len(arr)
     for i in range(1,n):
@@ -23,6 +24,32 @@ def insertion(arr):
         while arr[i-1]>current and i>0:
             arr[i-1], arr[i] = arr[i],arr[i-1]
             i -= 1
+
+# ----------------------------------- Merge sort
+def merge(left, right):
+    new = []
+    i,j = 0,0
+    while i<len(left) and j<len(right):
+        if left[i]<right[j]:
+            new.append(left[i])
+            i+=1
+        else:
+            new.append(right[j])
+            j+=1
+    new += left[i:]
+    new += right[j:]
+    return new
+
+
+def merge_sort(arr):
+    if len(arr)<=1:
+        return arr
+    mid = len(arr)//2
+    left_array = arr[:mid]
+    right_array = arr[mid:]
+    left_array = merge_sort(left_array)
+    right_array = merge_sort(right_array)
+    return merge(left_array, right_array)
 
 
 
@@ -32,4 +59,5 @@ print("Unsorted Array: ",arr)
 # selection(arr)
 # bubble(arr)
 # insertion(arr)
-print("Sorted Array: ",arr)
+print("Sorted Array: ", merge_sort(arr))
+# print("Sorted Array: ",arr)
